9f9340004d746d6290b968bc1d772694c08a6f73
ADITHYAN.A <113985941+adithya0808@users.noreply.github.com>
2024-03-09 22:34:13 +0530

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -4,14 +4,14 @@ import NewsItems from './NewsItems';
 const NewsBoard = ({category}) => {
     const [articles,setArticles]=useState([]);
     useEffect(()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${"a562a88e8c904d4a94319b5e7ce46397"}`
+        let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=a562a88e8c904d4a94319b5e7ce46397}`
         fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
     },[category])
   return (
     <div className='text-center'>
         <h2>Latest <span className='badge bg-danger'>News</span></h2>
         {articles.map((news,index)=>{
-            return <NewsItems Key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+            return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
         })}
     </div>
   )
