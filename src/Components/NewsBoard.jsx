740d3815b2224d0c518798669fda5c093b8e8712
ADITHYAN.A <113985941+adithya0808@users.noreply.github.com>
2024-03-22 22:12:28 +0530

    Updating

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -2,31 +2,14 @@ import React, { useEffect, useState } from 'react'
 import NewsItems from './NewsItems';
 
 const NewsBoard = ({category}) => {
-    const [articles,setArticles]=useState([]);
-   useEffect(() => {
-        const fetchNews = async () => {
-            try {
-                let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=a562a88e8c904d4a94319b5e7ce46397`;
-
-                const response = await fetch(url);
-
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const data = await response.json();
-                setArticles(data.articles);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchNews();
-    }, [category]);
+    const [state, setState] = useState([]);
+    const [hasError, setHasError] = useState(false);
+    
+    //a562a88e8c904d4a94319b5e7ce46397
   return (
     <div className='text-center'>
         <h2>Latest <span className='badge bg-danger'>News</span></h2>
-        {articles.map((news,index)=>{
+        {state && state.map((news,index)=>{
         <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
         })}
     </div>
