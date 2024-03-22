9c416fb105b996f23060111c65df24cefe7c0ab2
ADITHYAN.A <113985941+adithya0808@users.noreply.github.com>
2024-03-22 22:24:15 +0530

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -4,8 +4,27 @@ import NewsItems from './NewsItems';
 const NewsBoard = ({category}) => {
     const [state, setState] = useState([]);
     const [hasError, setHasError] = useState(false);
+    const fetchData = () =>{
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=a562a88e8c904d4a94319b5e7ce46397`)
+    .then(response =>response.json())
+    .then(res => {
+
+
+        setState(res.articles)
+        console.log(res);
+    }
+        
+        )
+    .catch(err => setHasError(true))
+
+}
+
+
+useEffect(()=>{
+fetchData();
+},[category])
+
     
-    //a562a88e8c904d4a94319b5e7ce46397
   return (
     <div className='text-center'>
         <h2>Latest <span className='badge bg-danger'>News</span></h2>
